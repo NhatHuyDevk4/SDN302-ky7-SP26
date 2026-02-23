7ac56c91a299ee56d6a310dd391af4c176c5ef84
NhatHuyDev <lenhathuy9a6@gmail.com>
2026-02-23 20:41:56 +0700

    Add rate limiting to category retrieval route

--- src/routes/category.route.js
+++ src/routes/category.route.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { protect } from "../middlewares/auth.middleware.js";
 import { createCategoryService, deleteCategoryService, getDetailCategoryService, getListCategory, updateCategoryService } from "../controllers/category.controller.js";
-
+import { userLimiter } from "../middlewares/rateLimit.middleware.js";
 
 const router = express();
 
 // [GET] /api/categories
-router.get("/", getListCategory)
+router.get("/", userLimiter, getListCategory)
 
 // [GET] /api/categories/:categoryId
 router.get("/:categoryId", getDetailCategoryService)
@@ -19,7 +19,5 @@ router.put("/:categoryId", protect, updateCategoryService)
 
 // [DELETE] /api/categories/:categoryId
 router.delete("/:categoryId", protect, deleteCategoryService)
-
-
 
 export default router;
